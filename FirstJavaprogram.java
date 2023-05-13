b54766e463ae55709f1ada60fcc930b2d2f5f660
‘abhixsh’ <aloka.abishek9@gmail.com>
2023-05-13 08:36:55 +0530

    Java fiest program comments added

--- FirstJavaprogram.java
+++ FirstJavaprogram.java
@@ -1,4 +1,11 @@
 public class FirstJavaprogram {
+
+    //single line comment
+    /* This is a 
+
+    
+    multi-line comment */
+
     public static void main (String[]args){
         System.out.println(" My first Java program.");
     }
