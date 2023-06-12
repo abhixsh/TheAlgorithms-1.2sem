0b1482b49c008be421b9261d2c235bbed7042240
‘abhixsh’ <aloka.abishek9@gmail.com>
2023-06-12 10:50:32 +0530

    6/12 ex

---
+++ votingEligibility.java
@@ -0,0 +1,12 @@
+public class votingEligibility {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18){
+            System.out.println("You are eligibility to vote.");
+        }
+        else{
+            System.out.println("You are not eligibility to vote.");
+        }
+    }
+}
+
