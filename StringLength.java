7d1a91ef9c4d3fe5125242161803bcd5ae30e27c
‘abhixsh’ <aloka.abishek9@gmail.com>
2023-06-12 08:30:08 +0530

    String Length

---
+++ StringLength.java
@@ -0,0 +1,7 @@
+public class StringLength {
+     public static void main(String[] args) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int length = txt.length();
+        System.out.println("The Length of the txt string is : " + length);
+    }
+}
